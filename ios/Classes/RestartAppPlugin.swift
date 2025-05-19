396d572a4cae36d3b6881c3e5f3eb6c60de9171d
joseph-lewis <42229769+joseph-lewis@users.noreply.github.com>
2025-05-19 15:00:01 +1000

    Fix IOS build bug

--- ios/Classes/RestartAppPlugin.swift
+++ ios/Classes/RestartAppPlugin.swift
@@ -11,12 +11,14 @@ public class RestartAppPlugin: NSObject, FlutterPlugin {
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         if call.method == "restartApp" {
             DispatchQueue.main.async {
-                if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
-                    appDelegate.restartFlutterApp()
+                if let appDelegate = UIApplication.shared.delegate,
+                   appDelegate.responds(to: Selector(("restartFlutterApp"))) {
+                    appDelegate.perform(Selector(("restartFlutterApp")))
                     result("ok")
                 } else {
-                    result(FlutterError(code: "APP_DELEGATE_NOT_FOUND", message: "Could not find AppDelegate", details: nil))
+                    result(FlutterError(code: "APP_DELEGATE_NOT_FOUND", message: "Could not find AppDelegate or method", details: nil))
                 }
+
             }
         } else {
             result(FlutterMethodNotImplemented)
